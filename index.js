3116266a6e8ac20d1716aab6fdcab1ec1966d0d0
First Last <flatironcamp.tc99@mamber.net>
2018-01-15 05:52:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Add your doToElementsInArray() function here:
 function doToElementsInArray(array, func) {
+  console.log("in doToElementsInArray()");
   array.forEach(func);
 }
 
 
 // Add your changeCompletely() function here:
 function changeCompletely(element, index, array) {
-  
+  console.log("in changeCompletely()");
   array[index] = Math.floor(Math.random() * 100 + 2).toString()
     + ` ${array[index]}s!!!`;
 }
